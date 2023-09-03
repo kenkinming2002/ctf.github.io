b742440784f91edcd7acd16084d28911e995cb05
KenKwok <kenkinming2002@gmail.com>
2023-09-03 23:02:53 +0800

    Fewer square

--- payload.js
+++ payload.js
@@ -1,13 +1,13 @@
 var canvas = document.getElementById("overlay");
 var ctx = canvas.getContext("2d");
-for (let y=0; y<64; ++y) {
-  for (let x=0; x<64; ++x) {
+for (let y=0; y<16; ++y) {
+  for (let x=0; x<16; ++x) {
     if((x + y) % 2 == 0) {
       ctx.fillStyle = "blue";
     } else {
       ctx.fillStyle = "red";
     }
-    ctx.fillRect(x * canvas.width / 64.0, y * canvas.height / 64.0, canvas.width / 64.0, canvas.height / 64.0);
+    ctx.fillRect(x * canvas.width / 16.0, y * canvas.height / 16.0, canvas.width / 16.0, canvas.height / 16.0);
   }
 }
 
